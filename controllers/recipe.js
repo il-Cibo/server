63bda08508d36bef4d422bdb06cf406864eb50f7
rizkyodit <rizkyodit@gmail.com>
2020-12-11 22:38:20 +0700

    add recipe test

--- controllers/recipe.js
+++ controllers/recipe.js
@@ -1,9 +1,8 @@
 const { Recipe } = require('../models/')
 const { gql } = require('apollo-server')
 
-class RecipeController {
 
-  static typeDefs = gql`
+const typeDefs = gql`
     type Recipe {
       id: Int
       title: String
@@ -37,52 +36,54 @@ class RecipeController {
     }`
 
 
-  static resolvers = {
-    Query: {
-      recipe: async (_, args) => {
-        try {
-          const data = await Recipe.findOne({ where: { id: args.id } })
-          return data
-        } catch (error) {
-          console.log(error)
-        }
-      },
-      recipes: async () => {
-        try {
-          const data = await Recipe.findAll()
-          return data
-        } catch (error) {
-          console.log(error)
-        }
+const resolvers = {
+  Query: {
+    recipe: async (_, args) => {
+      try {
+        const data = await Recipe.findOne({ where: { id: args.id } })
+        return data
+      } catch (error) {
+        console.log(error)
       }
     },
-    Mutation: {
-      addRecipe: async (_, args) => {
-        try {
-          const data = await Recipe.create(args.recipe)
-          return data
-        } catch (error) {
-          console.log(error)
-        }
-      },
-      editRecipe: async (_, args) => {
-        try {
-          const data = await Recipe.update(args.recipe, { where: { id: args.id }, returning: true })
-          return data[1][0].dataValues
-        } catch (error) {
-          console.log(error)
-        }
-      },
-      deleteRecipe: async (_, args) => {
-        try {
-          await Recipe.destroy({ where: { id: args.id } })
-          return { message: 'Recipe has been deleted' }
-        } catch (error) {
-          console.log(error)
-        }
+    recipes: async () => {
+      try {
+        const data = await Recipe.findAll()
+        return data
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  },
+  Mutation: {
+    addRecipe: async (_, args) => {
+      try {
+        const data = await Recipe.create(args.recipe)
+        return data
+      } catch (error) {
+        console.log(error)
+      }
+    },
+    editRecipe: async (_, args) => {
+      try {
+        const data = await Recipe.update(args.recipe, { where: { id: args.id }, returning: true })
+        return data[1][0].dataValues
+      } catch (error) {
+        console.log(error)
+      }
+    },
+    deleteRecipe: async (_, args) => {
+      try {
+        await Recipe.destroy({ where: { id: args.id } })
+        return { message: 'Recipe has been deleted' }
+      } catch (error) {
+        console.log(error)
       }
     }
   }
 }
 
-module.exports = RecipeController
+module.exports = {
+  typeDefs,
+  resolvers
+}
