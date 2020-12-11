23b20e91d08b9d2cbe6d779cb0572648030b5325
rizkyodit <rizkyodit@gmail.com>
2020-12-11 17:44:05 +0700

    test

--- app.js
+++ app.js
@@ -1,20 +1,112 @@
-const { ApolloServer, gql, makeExecutableSchema } = require('apollo-server')
+const { ApolloServer, gql } = require('apollo-server')
 
 const typeDefs = gql`
-  type Query
-  type Mutation
+  type User {
+    username: String
+    email: String
+    password: String
+    gender: String
+    name: String
+    avatar: String
+  }
+  type Recipe {
+    title: String
+    description: String
+    image: String
+    ingredients: [String]
+    step: [String]
+  }
+  type Tag {
+    name: String
+  }
+  type Response {
+    message: String
+  }
+  input NewUser {
+    username: String!
+    email: String!
+    password: String!
+    gender: String!
+    name: String!
+    avatar: String!
+  }
+  input NewRecipe {
+    title: String!
+    description: String!
+    image: String!
+    ingredients: [String!]
+    step: [String!]
+  }
+  type Query {
+    login: User
+    users: [User]
+    recipe: Recipe
+    recipes: [Recipe]
+    tag: Tag
+  }
+  type Mutation {
+    register(user: NewUser): User
+    editUser(user: NewUser): User
+    deleteUser(id: ID!): Response
+
+    addRecipe(recipe: NewRecipe): Recipe
+    editRecipe(recipe: NewRecipe): Recipe
+    deleteRecipe(id: ID!): Response
+
+    addTag(tag: String): Tag
+    deleteTag(id: ID!): Response
+  }
 `
 
-const schema = makeExecutableSchema({
-  typeDefs: [
-    typeDefs,
-  ],
-  resolvers: [
-  ]
-})
+const resolvers = {
+  Query: {
+    login: async () => {
+
+    },
+    users: async () => {
+
+    },
+    recipe: async () => {
+
+    },
+    recipes: async () => {
+
+    },
+    tag: async () => {
+
+    }
+  },
+  Mutation: {
+    register: async (_, args) => {
+
+    },
+    editUser: async (_, args) => {
+
+    },
+    deleteUser: async (_, args) => {
+
+    },
+    addRecipe: async (_, args) => {
+
+    },
+    editRecipe: async (_, args) => {
+
+    },
+    deleteRecipe: async (_, args) => {
+
+    },
+    addTag: async (_, args) => {
+
+    },
+    deleteTag: async (_, args) => {
+
+    }
+  }
+};
 
 const server = new ApolloServer({
-  schema
+  typeDefs,
+  resolvers
 });
 
 server.listen().then(({ url }) => {
