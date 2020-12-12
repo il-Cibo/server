9f9961dbf1de7b74a01211a93fdf5bbd40f60765
Gerry Anthony <gerrysimangunsong@gmail.com>
2020-12-12 10:07:11 +0700

    findOrCreate tag

--- controllers/tag.js
+++ controllers/tag.js
@@ -2,7 +2,12 @@ const { Tag, Recipe, User } = require('../models');
 
 class TagController {
   static async create (name) {
-    return await Tag.create({ name });
+    name = name.trim().toLowerCase();
+    return await Tag.findOrCreate({
+      where: {
+        name
+      }
+    });
   }
 
   static async find(id) {
